3fe56df877385fe4b4c4877fd45acc2e34e51db4
ULADZIMIR BALASHENKA <vova.bolashenko@gmail.com>
2025-02-19 21:44:18 +0300

    реализовано применение операторов Собеля и Приветта

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,36 @@
+#include <iostream>
+#include <filesystem>
+#include "kernel.h"
+#include "tiff_image.h"
+#include <string>
+
+namespace fs = std::filesystem;
+
 int main() {
+  fs::path project_source_dir(PROJECT_SOURCE_DIR);
+  if (!fs::exists(project_source_dir / "images")) {
+    fs::create_directory(project_source_dir / "images");
+    fs::create_directory(project_source_dir / "images/original");
+    fs::create_directory(project_source_dir / "images/prewitt");
+    fs::create_directory(project_source_dir / "images/sobel");
+    std::cout << "Поместите исходные изображения в "
+              << project_source_dir / "images/original" << std::endl;
+    return 0;
+  }
+  fs::path original_images_dir(project_source_dir / "images/original");
+  fs::path prewitt_images_dir(project_source_dir / "images/prewitt");
+  fs::path sobel_images_dir(project_source_dir / "images/sobel");
+  for (const auto& entry : fs::directory_iterator(original_images_dir)) {
+    if (entry.is_regular_file()) {
+      std::string image_name = entry.path().filename().string();
+      std::string image_path = entry.path().string();
+      TIFFImage<uint16_t> image(image_path);
+      TIFFImage<uint16_t> prewitt_image = image.SetKernel(kKernelPrewitt);
+      prewitt_image.Save(prewitt_images_dir / image_name);
+      TIFFImage<uint16_t> sobel_image = image.SetKernel(kKernelSobel);
+      sobel_image.Save(sobel_images_dir / image_name);
+    }
+  }
+  std::cout << "Изображения обработаны" << std::endl;
+  return 0;
 }
