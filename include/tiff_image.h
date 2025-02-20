addb8820cd345ba8a6ad70cbcdf9eaaf9a6f7c53
ULADZIMIR BALASHENKA <vova.bolashenko@gmail.com>
2025-02-20 18:28:59 +0300

    шаблонный класс переписан на uint16_t

--- include/tiff_image.h
+++ include/tiff_image.h
@@ -11,17 +11,13 @@
 #include "kernel.h"
 #include <cstddef>
 #include <cstdint>
-#include <stdexcept>
 #include <string>
 #include <tiff.h>
 #include <tiffio.h>
 
 /**
  * @brief Класс для работы с TIFF изображением.
- *
- * @tparam T Тип данных изображения (8 или 16 бит).
  */
-template <typename T>
 class TIFFImage {
  private:
   TIFF* tif_ = nullptr;  ///< Указатель на объект TIFF.
@@ -31,14 +27,15 @@ class TIFFImage {
   bool photo_metric_enabled_ = true,
        resolution_unit_enabled_ = true;  ///< Флаги включения параметров.
   float resolution_x_, resolution_y_;  ///< Разрешение по осям X и Y.
-  T** image_ = nullptr;  ///< Двумерный массив, представляющий изображение.
+  uint16_t** image_ =
+      nullptr;  ///< Двумерный массив, представляющий изображение.
 
   /**
    * @brief Меняет порядок байтов в массиве.
    *
    * @param array Массив данных.
    */
-  void SwapBytes(T* array);
+  void SwapBytes(uint16_t* array);
 
  public:
   /**
@@ -75,7 +72,7 @@ class TIFFImage {
    *
    * @param other Исходный объект TIFFImage для копирования.
    */
-  TIFFImage(const TIFFImage<T>& other);
+  TIFFImage(const TIFFImage& other);
 
   /**
    * @brief Деструктор.
@@ -138,7 +135,7 @@ class TIFFImage {
    * @return Значение пикселя.
    * @throws std::runtime_error Если изображение не загружено.
    */
-  T Get(const int x, const int y) const noexcept(false);
+  uint16_t Get(const int x, const int y) const noexcept(false);
 
   /**
    * @brief Устанавливает значение пикселя.
@@ -149,14 +146,15 @@ class TIFFImage {
    * @throws std::runtime_error Если изображение не загружено или координаты
    * выходят за границы.
    */
-  void Set(const size_t x, const size_t y, const T value) noexcept(false);
+  void Set(const size_t x, const size_t y,
+           const uint16_t value) noexcept(false);
 
   /**
    * @brief Копирует поля из другого объекта TIFFImage.
    *
    * @param other Исходный объект TIFFImage для копирования.
    */
-  void CopyFields(const TIFFImage<T>& other);
+  void CopyFields(const TIFFImage& other);
 
   /**
    * @brief Оператор сравнения.
@@ -166,7 +164,7 @@ class TIFFImage {
    * @param other Объект для сравнения.
    * @return true, если объекты равны, иначе false.
    */
-  bool operator==(const TIFFImage<T>& other) const;
+  bool operator==(const TIFFImage& other) const;
 
   /**
    * @brief Оператор присваивания.
@@ -176,7 +174,7 @@ class TIFFImage {
    * @param other Исходный объект TIFFImage для копирования.
    * @return Ссылка на текущий объект.
    */
-  TIFFImage<T>& operator=(const TIFFImage<T>& other);
+  TIFFImage& operator=(const TIFFImage& other);
 
   /**
    * @brief Применяет ядро к изображению.
@@ -187,276 +185,5 @@ class TIFFImage {
    * @param rotate Флаг, указывающий, нужно ли поворачивать ядро.
    * @return Новое изображение с примененным ядром.
    */
-  TIFFImage<T> SetKernel(const Kernel& kernel, bool rotate = true) const;
+  TIFFImage SetKernel(const Kernel& kernel, bool rotate = true) const;
 };
-
-template <typename T>
-void TIFFImage<T>::SwapBytes(T* array) {
-  if (sizeof(T) != 2) {
-    return;
-  }
-  for (size_t i = 0; i < width_; i++) {
-    array[i] = (array[i] >> 8) | (array[i] << 8);
-  }
-}
-
-template <typename T>
-TIFFImage<T>::TIFFImage() {
-}
-
-template <typename T>
-TIFFImage<T>::TIFFImage(const char* name) noexcept(false) {
-  Open(name);
-}
-
-template <typename T>
-TIFFImage<T>::TIFFImage(const std::string name) noexcept(false) {
-  Open(name);
-}
-
-template <typename T>
-TIFFImage<T>::TIFFImage(const TIFFImage<T>& other) {
-  CopyFields(other);
-  for (size_t i = 0; i < height_; i++) {
-    for (size_t j = 0; j < width_; j++) {
-      image_[i][j] = other.image_[i][j];
-    }
-  }
-}
-
-template <typename T>
-TIFFImage<T>::~TIFFImage() {
-  Close();
-  if (image_ != nullptr) {
-    for (size_t i = 0; i < height_; i++) {
-      delete[] image_[i];
-    }
-    delete[] image_;
-  }
-}
-
-template <typename T>
-void TIFFImage<T>::Open(const char* name) noexcept(false) {
-  tif_ = TIFFOpen(name, "r");
-  if (tif_ == nullptr) {
-    throw std::runtime_error("Невозможно открыть файл");
-  }
-  TIFFGetField(tif_, TIFFTAG_IMAGEWIDTH, &width_);
-  TIFFGetField(tif_, TIFFTAG_IMAGELENGTH, &height_);
-  TIFFGetField(tif_, TIFFTAG_SAMPLESPERPIXEL, &samples_per_pixel_);
-  TIFFGetField(tif_, TIFFTAG_BITSPERSAMPLE, &bits_per_sample_);
-  TIFFGetField(tif_, TIFFTAG_PLANARCONFIG, &config_);
-  if (TIFFGetField(tif_, TIFFTAG_PHOTOMETRIC, &photo_metric_) != 1) {
-    photo_metric_enabled_ = false;
-  }
-  if (TIFFGetField(tif_, TIFFTAG_RESOLUTIONUNIT, &resolution_unit_) != 1) {
-    resolution_unit_enabled_ = false;
-  }
-  if (TIFFGetField(tif_, TIFFTAG_XRESOLUTION, &resolution_x_) == 0) {
-    resolution_x_ = -1;
-  }
-  if (TIFFGetField(tif_, TIFFTAG_YRESOLUTION, &resolution_y_) == 0) {
-    resolution_y_ = -1;
-  }
-  image_ = new T*[height_];
-  for (size_t i = 0; i < height_; i++) {
-    image_[i] = new T[width_];
-    TIFFReadScanline(tif_, image_[i], i);
-    // SwapBytes(image_[i]);
-  }
-}
-
-template <typename T>
-void TIFFImage<T>::Open(const std::string name) noexcept(false) {
-  Open(name.c_str());
-}
-
-template <typename T>
-void TIFFImage<T>::Close() {
-  if (tif_ != nullptr) {
-    TIFFClose(tif_);
-    tif_ = nullptr;
-  }
-}
-
-template <typename T>
-void TIFFImage<T>::Save(const char* name) {
-  tif_ = TIFFOpen(name, "w");
-  if (tif_ == nullptr) {
-    throw std::runtime_error("Невозможно создать файл");
-  }
-  TIFFSetField(tif_, TIFFTAG_IMAGEWIDTH, width_);
-  TIFFSetField(tif_, TIFFTAG_IMAGELENGTH, height_);
-  TIFFSetField(tif_, TIFFTAG_SAMPLESPERPIXEL, samples_per_pixel_);
-  TIFFSetField(tif_, TIFFTAG_BITSPERSAMPLE, bits_per_sample_);
-  TIFFSetField(tif_, TIFFTAG_PLANARCONFIG, config_);
-  if (photo_metric_enabled_) {
-    TIFFSetField(tif_, TIFFTAG_PHOTOMETRIC, photo_metric_);
-  }
-  if (resolution_unit_enabled_) {
-    TIFFSetField(tif_, TIFFTAG_RESOLUTIONUNIT, resolution_unit_);
-  }
-  if (resolution_x_ != -1) {
-    TIFFSetField(tif_, TIFFTAG_XRESOLUTION, resolution_x_);
-  }
-  if (resolution_y_ != -1) {
-    TIFFSetField(tif_, TIFFTAG_YRESOLUTION, resolution_y_);
-  }
-  for (size_t i = 0; i < height_; i++) {
-    // SwapBytes(image_[i]);
-    TIFFWriteScanline(tif_, image_[i], i);
-  }
-  TIFFClose(tif_);
-  tif_ = nullptr;
-}
-
-template <typename T>
-void TIFFImage<T>::Save(const std::string name) {
-  Save(name.c_str());
-}
-
-template <typename T>
-void TIFFImage<T>::Clear() {
-  if (image_ != nullptr) {
-    for (size_t i = 0; i < height_; i++) {
-      delete[] image_[i];
-    }
-    delete[] image_;
-    image_ = nullptr;
-  }
-  width_ = 0;
-  height_ = 0;
-  samples_per_pixel_ = 0;
-  bits_per_sample_ = 0;
-  photo_metric_ = 0;
-  resolution_unit_ = 0;
-  config_ = 0;
-  photo_metric_enabled_ = true;
-  resolution_unit_enabled_ = true;
-  resolution_x_ = 0;
-  resolution_y_ = 0;
-}
-
-template <typename T>
-T TIFFImage<T>::Get(const int x, const int y) const noexcept(false) {
-  if ((width_ != 0u) && (height_ != 0u) && image_) {
-    if (x < 0 || x >= static_cast<int>(width_) || y < 0 ||
-        y >= static_cast<int>(height_)) {
-      return 0;
-    }
-    return image_[y][x];
-  } else {
-    throw std::runtime_error("Изображение не загружено");
-  }
-}
-
-template <typename T>
-void TIFFImage<T>::Set(const size_t x, const size_t y,
-                       const T value) noexcept(false) {
-  if ((width_ != 0u) && (height_ != 0u) && image_) {
-    if (x < width_ || x >= width_ || y < height_ || y >= height_) {
-      throw std::runtime_error("Выход за границы изображения");
-    }
-    image_[y][x] = value;
-  } else {
-    throw std::runtime_error("Изображение не загружено");
-  }
-}
-
-template <typename T>
-void TIFFImage<T>::CopyFields(const TIFFImage<T>& other) {
-  Clear();
-  width_ = other.width_;
-  height_ = other.height_;
-  samples_per_pixel_ = other.samples_per_pixel_;
-  bits_per_sample_ = other.bits_per_sample_;
-  photo_metric_ = other.photo_metric_;
-  resolution_unit_ = other.resolution_unit_;
-  config_ = other.config_;
-  photo_metric_enabled_ = other.photo_metric_enabled_;
-  resolution_unit_enabled_ = other.resolution_unit_enabled_;
-  resolution_x_ = other.resolution_x_;
-  resolution_y_ = other.resolution_y_;
-  image_ = new T*[height_];
-  for (size_t i = 0; i < height_; i++) {
-    image_[i] = new T[width_];
-    for (size_t j = 0; j < width_; j++) {
-      image_[i][j] = other.image_[i][j];
-    }
-  }
-}
-
-template <typename T>
-bool TIFFImage<T>::operator==(const TIFFImage<T>& other) const {
-  if (width_ != other.width_ || height_ != other.height_ ||
-      samples_per_pixel_ != other.samples_per_pixel_ ||
-      bits_per_sample_ != other.bits_per_sample_ ||
-      photo_metric_ != other.photo_metric_ ||
-      resolution_unit_ != other.resolution_unit_ || config_ != other.config_ ||
-      photo_metric_enabled_ != other.photo_metric_enabled_ ||
-      resolution_unit_enabled_ != other.resolution_unit_enabled_ ||
-      resolution_x_ != other.resolution_x_ ||
-      resolution_y_ != other.resolution_y_) {
-    return false;
-  }
-
-  for (size_t i = 0; i < height_; i++) {
-    for (size_t j = 0; j < width_; j++) {
-      if (image_[i][j] != other.image_[i][j]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
-template <typename T>
-TIFFImage<T>& TIFFImage<T>::operator=(const TIFFImage<T>& other) {
-  if (this == &other) {
-    return *this;
-  }
-  CopyFields(other);
-  for (size_t i = 0; i < height_; i++) {
-    for (size_t j = 0; j < width_; j++) {
-      image_[i][j] = other.image_[i][j];
-    }
-  }
-  return *this;
-}
-
-template <typename T>
-TIFFImage<T> TIFFImage<T>::SetKernel(const Kernel& kernel, bool rotate) const {
-  TIFFImage<T> result(*this);
-  if (rotate) {
-    Kernel kernel_y(kernel);
-    kernel_y.Rotate(KernelRotationDegrees::DEGREES_90);
-    for (size_t i = 0; i < height_; i++) {
-      for (size_t j = 0; j < width_; j++) {
-        int g_x = 0, g_y = 0;
-        int radius = kernel.GetSize() / 2;
-        for (int k = -radius; k <= radius; k++) {
-          for (int l = -radius; l <= radius; l++) {
-            g_x += kernel.Get(k + radius, l + radius) * Get(i + k, j + l);
-            g_y += kernel_y.Get(k + radius, l + radius) * Get(i + k, j + l);
-          }
-        }
-        result.image_[i][j] = abs(g_x) + abs(g_y);
-      }
-    }
-  } else {
-    for (size_t i = 0; i < height_; i++) {
-      for (size_t j = 0; j < width_; j++) {
-        int g = 0;
-        int radius = kernel.GetSize() / 2;
-        for (int k = -radius; k <= radius; k++) {
-          for (int l = -radius; l <= radius; l++) {
-            g += kernel.Get(k + radius, l + radius) * Get(i + k, j + l);
-          }
-        }
-        result.image_[i][j] = abs(g);
-      }
-    }
-  }
-  return result;
-}
---
+++ src/tiff_image.cpp
@@ -0,0 +1,255 @@
+#include "tiff_image.h"
+#include <stdexcept>
+
+void TIFFImage::SwapBytes(uint16_t* array) {
+  if (sizeof(uint16_t) != 2) {
+    return;
+  }
+  for (size_t i = 0; i < width_; i++) {
+    array[i] = (array[i] >> 8) | (array[i] << 8);
+  }
+}
+
+TIFFImage::TIFFImage() {
+}
+
+TIFFImage::TIFFImage(const char* name) noexcept(false) {
+  Open(name);
+}
+
+TIFFImage::TIFFImage(const std::string name) noexcept(false) {
+  Open(name);
+}
+
+TIFFImage::TIFFImage(const TIFFImage& other) {
+  CopyFields(other);
+  for (size_t i = 0; i < height_; i++) {
+    for (size_t j = 0; j < width_; j++) {
+      image_[i][j] = other.image_[i][j];
+    }
+  }
+}
+
+TIFFImage::~TIFFImage() {
+  Close();
+  if (image_ != nullptr) {
+    for (size_t i = 0; i < height_; i++) {
+      delete[] image_[i];
+    }
+    delete[] image_;
+  }
+}
+
+void TIFFImage::Open(const char* name) noexcept(false) {
+  tif_ = TIFFOpen(name, "r");
+  if (tif_ == nullptr) {
+    throw std::runtime_error("Невозможно открыть файл");
+  }
+  TIFFGetField(tif_, TIFFTAG_IMAGEWIDTH, &width_);
+  TIFFGetField(tif_, TIFFTAG_IMAGELENGTH, &height_);
+  TIFFGetField(tif_, TIFFTAG_SAMPLESPERPIXEL, &samples_per_pixel_);
+  TIFFGetField(tif_, TIFFTAG_BITSPERSAMPLE, &bits_per_sample_);
+  TIFFGetField(tif_, TIFFTAG_PLANARCONFIG, &config_);
+  if (TIFFGetField(tif_, TIFFTAG_PHOTOMETRIC, &photo_metric_) != 1) {
+    photo_metric_enabled_ = false;
+  }
+  if (TIFFGetField(tif_, TIFFTAG_RESOLUTIONUNIT, &resolution_unit_) != 1) {
+    resolution_unit_enabled_ = false;
+  }
+  if (TIFFGetField(tif_, TIFFTAG_XRESOLUTION, &resolution_x_) == 0) {
+    resolution_x_ = -1;
+  }
+  if (TIFFGetField(tif_, TIFFTAG_YRESOLUTION, &resolution_y_) == 0) {
+    resolution_y_ = -1;
+  }
+  image_ = new uint16_t*[height_];
+  for (size_t i = 0; i < height_; i++) {
+    image_[i] = new uint16_t[width_];
+    TIFFReadScanline(tif_, image_[i], i);
+    // SwapBytes(image_[i]);
+  }
+}
+
+void TIFFImage::Open(const std::string name) noexcept(false) {
+  Open(name.c_str());
+}
+
+void TIFFImage::Close() {
+  if (tif_ != nullptr) {
+    TIFFClose(tif_);
+    tif_ = nullptr;
+  }
+}
+
+void TIFFImage::Save(const char* name) {
+  tif_ = TIFFOpen(name, "w");
+  if (tif_ == nullptr) {
+    throw std::runtime_error("Невозможно создать файл");
+  }
+  TIFFSetField(tif_, TIFFTAG_IMAGEWIDTH, width_);
+  TIFFSetField(tif_, TIFFTAG_IMAGELENGTH, height_);
+  TIFFSetField(tif_, TIFFTAG_SAMPLESPERPIXEL, samples_per_pixel_);
+  TIFFSetField(tif_, TIFFTAG_BITSPERSAMPLE, bits_per_sample_);
+  TIFFSetField(tif_, TIFFTAG_PLANARCONFIG, config_);
+  if (photo_metric_enabled_) {
+    TIFFSetField(tif_, TIFFTAG_PHOTOMETRIC, photo_metric_);
+  }
+  if (resolution_unit_enabled_) {
+    TIFFSetField(tif_, TIFFTAG_RESOLUTIONUNIT, resolution_unit_);
+  }
+  if (resolution_x_ != -1) {
+    TIFFSetField(tif_, TIFFTAG_XRESOLUTION, resolution_x_);
+  }
+  if (resolution_y_ != -1) {
+    TIFFSetField(tif_, TIFFTAG_YRESOLUTION, resolution_y_);
+  }
+  for (size_t i = 0; i < height_; i++) {
+    // SwapBytes(image_[i]);
+    TIFFWriteScanline(tif_, image_[i], i);
+  }
+  TIFFClose(tif_);
+  tif_ = nullptr;
+}
+
+void TIFFImage::Save(const std::string name) {
+  Save(name.c_str());
+}
+
+void TIFFImage::Clear() {
+  if (image_ != nullptr) {
+    for (size_t i = 0; i < height_; i++) {
+      delete[] image_[i];
+    }
+    delete[] image_;
+    image_ = nullptr;
+  }
+  width_ = 0;
+  height_ = 0;
+  samples_per_pixel_ = 0;
+  bits_per_sample_ = 0;
+  photo_metric_ = 0;
+  resolution_unit_ = 0;
+  config_ = 0;
+  photo_metric_enabled_ = true;
+  resolution_unit_enabled_ = true;
+  resolution_x_ = 0;
+  resolution_y_ = 0;
+}
+
+uint16_t TIFFImage::Get(const int x, const int y) const noexcept(false) {
+  if ((width_ != 0u) && (height_ != 0u) && (image_ != nullptr)) {
+    if (x < 0 || x >= static_cast<int>(width_) || y < 0 ||
+        y >= static_cast<int>(height_)) {
+      return 0;
+    }
+    return image_[y][x];
+  } else {
+    throw std::runtime_error("Изображение не загружено");
+  }
+}
+
+void TIFFImage::Set(const size_t x, const size_t y,
+                    const uint16_t value) noexcept(false) {
+  if ((width_ != 0u) && (height_ != 0u) && (image_ != nullptr)) {
+    if (x < width_ || x >= width_ || y < height_ || y >= height_) {
+      throw std::runtime_error("Выход за границы изображения");
+    }
+    image_[y][x] = value;
+  } else {
+    throw std::runtime_error("Изображение не загружено");
+  }
+}
+
+void TIFFImage::CopyFields(const TIFFImage& other) {
+  Clear();
+  width_ = other.width_;
+  height_ = other.height_;
+  samples_per_pixel_ = other.samples_per_pixel_;
+  bits_per_sample_ = other.bits_per_sample_;
+  photo_metric_ = other.photo_metric_;
+  resolution_unit_ = other.resolution_unit_;
+  config_ = other.config_;
+  photo_metric_enabled_ = other.photo_metric_enabled_;
+  resolution_unit_enabled_ = other.resolution_unit_enabled_;
+  resolution_x_ = other.resolution_x_;
+  resolution_y_ = other.resolution_y_;
+  image_ = new uint16_t*[height_];
+  for (size_t i = 0; i < height_; i++) {
+    image_[i] = new uint16_t[width_];
+    for (size_t j = 0; j < width_; j++) {
+      image_[i][j] = other.image_[i][j];
+    }
+  }
+}
+
+bool TIFFImage::operator==(const TIFFImage& other) const {
+  if (width_ != other.width_ || height_ != other.height_ ||
+      samples_per_pixel_ != other.samples_per_pixel_ ||
+      bits_per_sample_ != other.bits_per_sample_ ||
+      photo_metric_ != other.photo_metric_ ||
+      resolution_unit_ != other.resolution_unit_ || config_ != other.config_ ||
+      photo_metric_enabled_ != other.photo_metric_enabled_ ||
+      resolution_unit_enabled_ != other.resolution_unit_enabled_ ||
+      resolution_x_ != other.resolution_x_ ||
+      resolution_y_ != other.resolution_y_) {
+    return false;
+  }
+
+  for (size_t i = 0; i < height_; i++) {
+    for (size_t j = 0; j < width_; j++) {
+      if (image_[i][j] != other.image_[i][j]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+TIFFImage& TIFFImage::operator=(const TIFFImage& other) {
+  if (this == &other) {
+    return *this;
+  }
+  CopyFields(other);
+  for (size_t i = 0; i < height_; i++) {
+    for (size_t j = 0; j < width_; j++) {
+      image_[i][j] = other.image_[i][j];
+    }
+  }
+  return *this;
+}
+
+TIFFImage TIFFImage::SetKernel(const Kernel& kernel, bool rotate) const {
+  TIFFImage result(*this);
+  if (rotate) {
+    Kernel kernel_y(kernel);
+    kernel_y.Rotate(KernelRotationDegrees::DEGREES_90);
+    for (size_t i = 0; i < height_; i++) {
+      for (size_t j = 0; j < width_; j++) {
+        int g_x = 0, g_y = 0;
+        int radius = kernel.GetSize() / 2;
+        for (int k = -radius; k <= radius; k++) {
+          for (int l = -radius; l <= radius; l++) {
+            g_x += kernel.Get(k + radius, l + radius) * Get(i + k, j + l);
+            g_y += kernel_y.Get(k + radius, l + radius) * Get(i + k, j + l);
+          }
+        }
+        result.image_[i][j] = abs(g_x) + abs(g_y);
+      }
+    }
+  } else {
+    for (size_t i = 0; i < height_; i++) {
+      for (size_t j = 0; j < width_; j++) {
+        int g = 0;
+        int radius = kernel.GetSize() / 2;
+        for (int k = -radius; k <= radius; k++) {
+          for (int l = -radius; l <= radius; l++) {
+            g += kernel.Get(k + radius, l + radius) * Get(i + k, j + l);
+          }
+        }
+        result.image_[i][j] = abs(g);
+      }
+    }
+  }
+  return result;
+}
