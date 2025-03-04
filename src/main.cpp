cf5e9691f881189bb85d3694383de767777d3aa0
ULADZIMIR BALASHENKA <vova.bolashenko@gmail.com>
2025-03-04 21:53:27 +0300

    добавлено применение фильтра Гаусса перед применением операторов Собеля и Превитта

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main() {
     fs::create_directory(project_source_dir / "images/original");
     fs::create_directory(project_source_dir / "images/prewitt");
     fs::create_directory(project_source_dir / "images/sobel");
+    fs::create_directory(project_source_dir / "images/gaussian");
     std::cout << "Поместите исходные изображения в "
               << project_source_dir / "images/original" << std::endl;
     return 0;
@@ -21,6 +22,31 @@ int main() {
   fs::path original_images_dir(project_source_dir / "images/original");
   fs::path prewitt_images_dir(project_source_dir / "images/prewitt");
   fs::path sobel_images_dir(project_source_dir / "images/sobel");
+  fs::path gaussian_images_dir(project_source_dir / "images/gaussian");
+
+  if (!fs::exists(original_images_dir)) {
+    fs::create_directory(project_source_dir / "images/original");
+    std::cerr << "Каталог с оригинальными изображениями отсутствует. Поместите "
+                 "исходные изображения в "
+              << original_images_dir << std::endl;
+    return 1;
+  }
+  for (const auto& entry : fs::directory_iterator(gaussian_images_dir)) {
+    if (entry.is_regular_file()) {
+      fs::remove(entry);
+    }
+  }
+  for (const auto& entry : fs::directory_iterator(prewitt_images_dir)) {
+    if (entry.is_regular_file()) {
+      fs::remove(entry);
+    }
+  }
+  for (const auto& entry : fs::directory_iterator(sobel_images_dir)) {
+    if (entry.is_regular_file()) {
+      fs::remove(entry);
+    }
+  }
+
   for (const auto& entry : fs::directory_iterator(original_images_dir)) {
     if (entry.is_regular_file()) {
       std::string image_name = entry.path().filename().string();
@@ -37,7 +63,12 @@ int main() {
                   << image_name << std::endl;
         continue;
       }
-      TIFFImage prewitt_image = image.SetKernel(kKernelPrewitt);
+      TIFFImage gaussian_image = image.GaussianBlur(5);
+      if (!fs::exists(gaussian_images_dir)) {
+        fs::create_directory(gaussian_images_dir);
+      }
+      gaussian_image.Save(gaussian_images_dir / image_name);
+      TIFFImage prewitt_image = gaussian_image.SetKernel(kKernelPrewitt);
       if (!fs::exists(prewitt_images_dir)) {
         fs::create_directory(prewitt_images_dir);
       }
@@ -45,7 +76,7 @@ int main() {
       if (!fs::exists(sobel_images_dir)) {
         fs::create_directory(sobel_images_dir);
       }
-      TIFFImage sobel_image = image.SetKernel(kKernelSobel);
+      TIFFImage sobel_image = gaussian_image.SetKernel(kKernelSobel);
       sobel_image.Save(sobel_images_dir / image_name);
     }
   }
