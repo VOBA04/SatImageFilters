4c2923a18bae79302db8b2183ffdab15f89cac15
ULADZIMIR BALASHENKA <vova.bolashenko@gmail.com>
2025-03-20 02:02:08 +0300

    Сделан тест эффективности разных типов Гаусса, операторы выделения краев пока отключены

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "tiff_image.h"
 #include <stdexcept>
 #include <string>
+#include <chrono>
 
 namespace fs = std::filesystem;
 
@@ -62,39 +63,56 @@ int main() {
                   << image_name << std::endl;
         continue;
       }
-      TIFFImage gaussian_image = image.GaussianBlur(5);
+      TIFFImage gaussian_image = image.GaussianBlur(9, 5);
+      // TIFFImage gaussian_image = image.GaussianBlurSep(9, 5);
       if (!fs::exists(gaussian_images_dir)) {
         fs::create_directory(gaussian_images_dir);
       }
       gaussian_image.Save(gaussian_images_dir / image_name);
-      TIFFImage prewitt_image = gaussian_image.SetKernel(kKernelPrewitt);
-      if (!fs::exists(prewitt_images_dir)) {
-        fs::create_directory(prewitt_images_dir);
-      }
-      prewitt_image.Save(prewitt_images_dir / image_name);
-      if (!fs::exists(sobel_images_dir)) {
-        fs::create_directory(sobel_images_dir);
-      }
-      TIFFImage sobel_image = gaussian_image.SetKernel(kKernelSobel);
-      sobel_image.Save(sobel_images_dir / image_name);
-      if (fs::exists(kernel_path)) {
-        Kernel<int> arbitrary_kernel;
-        try {
-          arbitrary_kernel.SetFromFile(kernel_path);
-          TIFFImage arbitrary_image =
-              gaussian_image.SetKernel(arbitrary_kernel);
-          if (!fs::exists(arbitrary_kernel_dir)) {
-            fs::create_directory(arbitrary_kernel_dir);
-          }
-          arbitrary_image.Save(arbitrary_kernel_dir / image_name);
-        } catch (KernelException& e) {
-          std::cerr << "Ошибка при загрузке ядра из файла " << kernel_path
-                    << ": " << e.what() << std::endl;
-        } catch (...) {
-          std::cerr << "Неизвестная ошибка при загрузке ядра из файла "
-                    << kernel_path << std::endl;
-        }
-      }
+      auto start1 = std::chrono::high_resolution_clock::now();
+      image.GaussianBlur(9, 5);
+      auto end1 = std::chrono::high_resolution_clock::now();
+      auto start2 = std::chrono::high_resolution_clock::now();
+      image.GaussianBlurSep(9, 5);
+      auto end2 = std::chrono::high_resolution_clock::now();
+      std::cout << "GaussianBlur time: "
+                << std::chrono::duration_cast<std::chrono::milliseconds>(end1 -
+                                                                         start1)
+                       .count()
+                << std::endl;
+      std::cout << "GaussianBlurSep time: "
+                << std::chrono::duration_cast<std::chrono::milliseconds>(end2 -
+                                                                         start2)
+                       .count()
+                << std::endl;
+      // TIFFImage prewitt_image = gaussian_image.SetKernel(kKernelPrewitt);
+      // if (!fs::exists(prewitt_images_dir)) {
+      //   fs::create_directory(prewitt_images_dir);
+      // }
+      // prewitt_image.Save(prewitt_images_dir / image_name);
+      // if (!fs::exists(sobel_images_dir)) {
+      //   fs::create_directory(sobel_images_dir);
+      // }
+      // TIFFImage sobel_image = gaussian_image.SetKernel(kKernelSobel);
+      // sobel_image.Save(sobel_images_dir / image_name);
+      // if (fs::exists(kernel_path)) {
+      //   Kernel<int> arbitrary_kernel;
+      //   try {
+      //     arbitrary_kernel.SetFromFile(kernel_path);
+      //     TIFFImage arbitrary_image =
+      //         gaussian_image.SetKernel(arbitrary_kernel);
+      //     if (!fs::exists(arbitrary_kernel_dir)) {
+      //       fs::create_directory(arbitrary_kernel_dir);
+      //     }
+      //     arbitrary_image.Save(arbitrary_kernel_dir / image_name);
+      //   } catch (KernelException& e) {
+      //     std::cerr << "Ошибка при загрузке ядра из файла " << kernel_path
+      //               << ": " << e.what() << std::endl;
+      //   } catch (...) {
+      //     std::cerr << "Неизвестная ошибка при загрузке ядра из файла "
+      //               << kernel_path << std::endl;
+      //   }
+      // }
     }
   }
   std::cout << "Изображения обработаны" << std::endl;
